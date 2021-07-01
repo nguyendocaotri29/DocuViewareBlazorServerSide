4981ea4646d7404717ddb6ca26395a8ab4b2b02e
Cao Tri Do <tridc@techvn.mti.co.jp>
2021-07-01 14:26:28 +0700

    Add project files.

---
+++ DocuViewareBlazorServerSide/Startup.cs
@@ -0,0 +1,73 @@
+using DocuViewareBlazorServerSide.Data;
+using GdPicture14.WEB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DocuViewareBlazorServerSide
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<SessionService>();
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44342/") });
+
+            var assemblyPath = Assembly.GetExecutingAssembly().Location;
+            var path = System.IO.Path.GetDirectoryName(assemblyPath);
+            DocuViewareLicensing.RegisterKEY("0404403347189896460401864");
+            DocuViewareManager.SetupConfiguration(true, DocuViewareSessionStateMode.InProc,
+                path + "\\Cache",
+                "https://localhost:44342/", "api/DocuViewareREST");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
